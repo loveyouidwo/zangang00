5293201700d7e2f22d8953d11fea0381b5823321
昝纲 <39905311+loveyouidwo@users.noreply.github.com>
2018-09-03 21:02:58 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
+(function(){
+    login();
+})();
+function login(){
+    $("submit").onclick = function(){
+        $("userName").onblur = function(){
+            var value1 = $("userName").value;
+            if(isNaN(value1)){
+                alert("输入的账号和密码为空");
+            }else if(value1 === "2212056035"){
+                $("password").onblur = function(){
+                    var value2 = $("password").value;
+                    if(isNaN(value2)){
+                        alert("输入的账号和密码为空");
+                    }else if(value2 === "980708"){
+                        location.href = "pages/myIndex.html";
+                    }else{
+                        location.href = "pages/myLogin.html";
+                    }
+                }
+            }else{
+                location.href = "pages/myLogin.html";
+            }
+        }
+    }
+}
+
+function $(id){
+    return typeof id === "string" ? document.getElementById(id) : null;
+}
 
